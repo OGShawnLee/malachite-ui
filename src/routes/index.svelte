72f7b5eb8f31db252dfcb0793b80e8430030766f
OGShawnLee <changoloco.evg@gmail.com>
2022-04-16 14:04:49 +0000

    refactor: remove uneeded code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,2 @@
-<script>
-	import Render from '@components/render';
-
-	let value = 'tamales';
-</script>
-
 <h1>Welcome to Svelte Malachite-UI</h1>
 <p>A component library inspired by Tailwind Headless UI</p>
-
-<span>{value}</span>
-<Render as="input" bind:value />
