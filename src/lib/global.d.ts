382d45fccb6c8cd18f3239d95821b0b45809a5a7
OGShawnLee <changoloco.evg@gmail.com>
2022-04-16 12:41:42 +0000

    fix(ts): add missing type

--- src/lib/global.d.ts
+++ src/lib/global.d.ts
@@ -4,4 +4,8 @@ type ExtractContext<C, K extends keyof C> = OmitAllThisParameter<Pick<C, K>>;
 
 type Nullable<T> = T | null | undefined;
 
+type OmitAllThisParameter<T> = {
+	[P in keyof T]: OmitThisParameter<T[P]>;
+};
+
 type RenderElementTagName = keyof HTMLElementTagNameMap | 'slot';
