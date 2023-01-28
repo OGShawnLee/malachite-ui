3c8d70174c0f1eba5263306736594aa76f0b8ea8
OGShawnLee <changoloco.evg@gmail.com>
2023-01-28 15:46:47 +0000

    feat(hooks)!: generate uid instead of requiring index

--- src/lib/hooks/useComponentNaming.ts
+++ src/lib/hooks/useComponentNaming.ts
@@ -1,35 +1,27 @@
-import type { Expand } from '$lib/types';
 import { LIBRARY_NAME } from '$lib/core';
-import { isEmpty, isNumber, isString } from '$lib/predicate';
+import { isEmpty, isString } from '$lib/predicate';
 import { clearString } from '$lib/utils';
+import { nanoid } from 'nanoid';
 
-interface Options {
-	parent?: string | null;
-	name: string;
-	index: number;
-}
+const UID_LENGTH = 8;
 
-export function useComponentNaming({ parent, name, index }: Expand<Options>): {
-	baseName: string;
-	index: number;
-	nameChild(childName: string, index?: number): string;
-} {
+export function useComponentNaming({ name, parent }: { name: string; parent?: string }) {
 	if (isString(parent) && isEmpty(parent)) throw Error('parent must not be an empty string.');
 	if (isEmpty(name)) throw Error('name must not be an empty string.');
 
+	const uid = nanoid(UID_LENGTH);
 	parent = parent ? separateWithDashes(parent) : parent;
 	name = separateWithDashes(name);
-	let baseName = `${parent || LIBRARY_NAME}-${name}-${index}`;
-	if (parent === null) baseName = `${name}-${index}`;
+	let baseName = `${parent || LIBRARY_NAME}-${name}-${uid}`;
+	if (parent === null) baseName = `${name}-${uid}`;
 	return {
 		baseName,
-		index,
-		nameChild(childName: string, index?: number) {
+		nameChild(childName: string) {
 			if (isEmpty(childName)) throw new Error('childName must not be an empty string.');
 
+			const uid = nanoid(UID_LENGTH);
 			childName = separateWithDashes(childName);
-			if (isNumber(index)) return `${baseName}-${childName}-${index}`;
-			return `${baseName}-${childName}`;
+			return `${baseName}-${childName}-${uid}`;
 		}
 	};
 }
