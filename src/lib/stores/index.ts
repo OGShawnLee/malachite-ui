3b780aec28eff353fadfa178a53ce2614355daa1
OGShawnLee <changoloco.evg@gmail.com>
2023-02-02 01:13:19 +0000

    refactor(stores)!: remove storable store

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -2,5 +2,4 @@ export * from './Group';
 export { default as Hashable } from './Hashable';
 export { default as Navigable } from './Navigable';
 export * from './Ordered';
-export * from './storable';
 export { Toggleable, ToggleableGroup } from './Toggleable';
