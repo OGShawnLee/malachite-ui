dc0f5d390a8e3dde11ab4747991999453aaabdff
OGShawnLee <changoloco.evg@gmail.com>
2023-02-02 01:11:59 +0000

    refactor(stores)!: remove notifiable store

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,7 +1,6 @@
 export * from './Group';
 export { default as Hashable } from './Hashable';
 export { default as Navigable } from './Navigable';
-export * from './notifiable';
 export * from './Ordered';
 export * from './storable';
 export { Toggleable, ToggleableGroup } from './Toggleable';
