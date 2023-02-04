0b0b09eceea6d7937c31ebc529951496fc7a1738
OGShawnLee <changoloco.evg@gmail.com>
2023-02-04 18:30:11 +0000

    docs: update landing page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+  <title> Malachite UI - Unstyled UI components for Svelte </title>
+</svelte:head>
+
 <h1>Welcome to Svelte Malachite-UI</h1>
 <p>A component library inspired by Tailwind Headless UI</p>
