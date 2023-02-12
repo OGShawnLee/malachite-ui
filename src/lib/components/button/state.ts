37eb80bdad9795e52d26caa45cbd5e6a34a42e01
OGShawnLee <changoloco.evg@gmail.com>
2023-02-12 23:37:17 +0000

    feat(button): add toolbar synergy

--- src/lib/components/button/state.ts
+++ src/lib/components/button/state.ts
@@ -1,9 +1,11 @@
+import ToolbarContext from '../toolbar/context';
 import { defineActionComponent } from '$lib/core';
 import { useComponentNaming, useListener } from '$lib/hooks';
 import { ref } from '$lib/utils';
 
 export function createButtonState(initialValue: boolean) {
 	const isPressed = ref(initialValue);
+	const toolbar = ToolbarContext.getContext(false);
 	const { baseName } = useComponentNaming('button');
 
 	function createButton(id: string | undefined) {
@@ -14,6 +16,7 @@ export function createButtonState(initialValue: boolean) {
 				element.role = 'button';
 				element.setAttribute('type', 'button');
 				return [
+					toolbar?.item(element),
 					useListener(element, 'click', () => {
 						isPressed.update((isPressed) => !isPressed);
 					}),
