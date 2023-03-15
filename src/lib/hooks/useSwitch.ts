31cd13ee5c9ce4b8baf2f607d80e32896c8926ea
OGShawnLee <changoloco.evg@gmail.com>
2023-03-15 21:19:43 +0000

    perf(hooks): do not create a new object

--- src/lib/hooks/useSwitch.ts
+++ src/lib/hooks/useSwitch.ts
@@ -2,11 +2,9 @@ import type { Switch } from '$lib/types';
 import { ref } from '$lib/utils';
 
 export default function useSwitch(initialValue: boolean): Switch {
-	const isChecked = ref(initialValue);
-
-	function toggle() {
+	const isChecked = ref(initialValue) as Switch;
+	isChecked.toggle = () => {
 		isChecked.update((isChecked) => !isChecked);
-	}
-
-	return { ...isChecked, toggle };
+	};
+	return isChecked;
 }
