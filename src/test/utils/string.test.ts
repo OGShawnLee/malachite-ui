6bcba762cbe462032527c5ca999900114d5d4337
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-04-11 18:54:09 +0000

    test(utils): enable clearString fn tests

--- src/test/utils/string.test.ts
+++ src/test/utils/string.test.ts
@@ -1,15 +1,15 @@
 import { clearString } from '$lib/utils';
 
-describe.skip('clearString', () => {
-	it.skip('Should trim the given string', () => {
+describe('clearString', () => {
+	it('Should trim the given string', () => {
 		expect(clearString('    this is a string    ')).toBe('this is a string');
 	});
 
-	it.skip('Should clear unnecesary spaces between words', () => {
+	it('Should clear unnecesary spaces between words', () => {
 		expect(clearString('    this     is a    string    ')).toBe('this is a string');
 	});
 
-	it.skip('Should clear all white space', () => {
+	it('Should clear all white space', () => {
 		expect(
 			clearString(`    
         this    is
