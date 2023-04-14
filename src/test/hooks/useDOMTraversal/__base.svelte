72905ded039e175fcb60349321b8479641fadaa2
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-04-14 17:38:39 +0000

    test(hooks): enable and update useDOMTraversal tests

--- src/test/hooks/useDOMTraversal/__base.svelte
+++ src/test/hooks/useDOMTraversal/__base.svelte
@@ -1,7 +1,7 @@
-<div data-testid="container-element">
+<div data-testid="container">
   <button> First Button </button>
   <button> Second Button </button>
-  <div data-testid="nested-container">
+  <div data-testid="inner-container">
     Nested Container
     <button disabled> Nested Disabled Button </button>
   </div>
--- src/test/hooks/useDOMTraversal/useDOMTraversal.test.ts
+++ src/test/hooks/useDOMTraversal/useDOMTraversal.test.ts
@@ -1,45 +1,58 @@
 import Base from './__base.svelte';
 import { useDOMTraversal } from '$lib/hooks';
-import { cleanup, render } from '@testing-library/svelte';
-import { hasTagName } from '$lib/predicate';
+import { render } from '@testing-library/svelte';
+import { hasTagName, isHTMLElement } from '$lib/predicate';
 
-afterEach(() => cleanup());
-
-it.skip('Should return all the nested elements by default', async () => {
-	const { findByTestId } = render(Base);
-	const container = await findByTestId('container-element');
+it("Should take an element and return its children", () => {
+	const { getByTestId } = render(Base);
+	const container = getByTestId('container');
 	const elements = useDOMTraversal(container);
 	expect(elements).toHaveLength(7);
+	const innerContainer = getByTestId('inner-container');
+	const innerElements = useDOMTraversal(innerContainer)
+	expect(innerElements).toHaveLength(2)
+})
+
+it('Should include the element', () => {
+	const { getByTestId } = render(Base);
+	const container = getByTestId('container');
+	const elements = useDOMTraversal(container);
+	expect(elements.includes(container)).toBe(true);
+	const innerContainer = getByTestId('inner-container');
+	const innerElements = useDOMTraversal(innerContainer);
+	expect(innerElements.includes(innerContainer)).toBe(true);
 });
 
-it.skip('Should traverse the given container', async () => {
-	const { findByTestId } = render(Base);
-	const nested = await findByTestId('nested-container');
-	const buttons = useDOMTraversal(nested, (element) => hasTagName(element, 'button'));
-	expect(buttons).toHaveLength(1);
-});
+it("Should only return HTMLElements", () => {
+	const { getByTestId } = render(Base);
+	const container = getByTestId('container');
+	const elements = useDOMTraversal(container);
+	for (const child of elements) {
+		expect(isHTMLElement(child)).toBe(true)
+	}
+})
 
-describe.skip('predicate', () => {
-	it.skip('Should return only the elements that match the given predicate', async () => {
-		const { findByTestId } = render(Base);
-		const container = await findByTestId('container-element');
-		const buttons = useDOMTraversal(container, (element) => hasTagName(element, 'button'));
-		expect(buttons).toHaveLength(3);
+describe("predicate", () => {
+	it("Should take a predicate function that decides which children are returned", () => {
+		const { getByTestId } = render(Base)
+		const container = getByTestId("container")
+		const buttons = useDOMTraversal(container, child => hasTagName(child, "button"))
+		expect(buttons).toHaveLength(3)
+		const section = useDOMTraversal(container, child => hasTagName(child, "section"))
+		expect(section).toHaveLength(1)
+		const disabledButton = useDOMTraversal(container, child => {
+			return hasTagName(child, "button") && child.disabled
+		})
+		expect(disabledButton).toHaveLength(1)
+	})
 
-		const section = useDOMTraversal(container, (element) => hasTagName(element, 'section'));
-		expect(section).toHaveLength(1);
-	});
-
-	it.skip('Should pass the current element, index and the selected elements', async () => {
-		const func = vi.fn<[Element, Element[]]>(() => {});
-		const { findByTestId } = render(Base);
-		const container = await findByTestId('container-element');
-		const allElements = useDOMTraversal(container);
-		useDOMTraversal(container, func);
-
-		for (const [element, filtered] of func.mock.calls) {
-			expect(allElements).toContain(element);
-			expect(filtered).toEqual([]);
+	it("Should pass the current child", () => {
+		const { getByTestId } = render(Base)
+		const container = getByTestId('container');
+		const fn = vi.fn<[HTMLElement]>(() => true)
+		const children = useDOMTraversal(container, fn)
+		for (const [child] of fn.mock.calls) {
+			expect(children).toContain(child)
 		}
-	});
-});
+	})
+})
