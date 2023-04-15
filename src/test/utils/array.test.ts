b4d852dcef180a2982a37df1ad66dc4f9963c1b2
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-04-15 00:33:39 +0000

    test(utils): enable array tests

--- src/test/utils/array.test.ts
+++ src/test/utils/array.test.ts
@@ -1,33 +1,33 @@
 import * as array from '$lib/utils/array';
 import { isArray } from '$lib/predicate';
 
-describe.skip('generate', () => {
+describe('generate', () => {
 	const { generate } = array;
 
-	it.skip('Should return an array', () => {
+	it('Should return an array', () => {
 		expect(generate(3, 3)).toBeInstanceOf(Array);
 	});
 
-	it.skip('Should return an array of the given length filled with the given value', () => {
+	it('Should return an array of the given length filled with the given value', () => {
 		const arr = generate(10, 5);
 		expect(arr).toHaveLength(10);
 		expect(isArray(arr, (num): num is number => num === 5)).toBe(true);
 	});
 
-	describe.skip('function value', () => {
-		it.skip('Should fill the array with the return value', () => {
+	describe('function value', () => {
+		it('Should fill the array with the return value', () => {
 			const names = generate(5, () => 'James');
 			expect(names).toHaveLength(5);
 			expect(isArray(names, (name): name is string => name === 'James')).toBe(true);
 		});
 
-		it.skip('Should call the function as many times as the given length', () => {
+		it('Should call the function as many times as the given length', () => {
 			const func = vi.fn(() => {});
 			generate(10, func);
 			expect(func).toBeCalledTimes(10);
 		});
 
-		it.skip('Should pass the current index', () => {
+		it('Should pass the current index', () => {
 			const func = vi.fn(() => {});
 			generate(5, func);
 			expect(func.mock.calls.flat()).toEqual([0, 1, 2, 3, 4]);
@@ -35,26 +35,26 @@ describe.skip('generate', () => {
 	});
 });
 
-describe.skip('getFirstAndLast', () => {
+describe('getFirstAndLast', () => {
 	const { getFirstAndLast } = array;
 	const items = [0, 1, 2, 3];
 
-	it.skip('Should return the first and last in a tuple', () => {
+	it('Should return the first and last in a tuple', () => {
 		expect(getFirstAndLast(items)).toBeInstanceOf(Array);
 		expect(getFirstAndLast(items)).toHaveLength(2);
 	});
 
-	it.skip('Should return the first and last item', () => {
+	it('Should return the first and last item', () => {
 		expect(getFirstAndLast(items)).toEqual([0, 3]);
 	});
 
-	it.skip('Should return the first item and undefined if the array has only one item', () => {
+	it('Should return the first item and undefined if the array has only one item', () => {
 		expect(getFirstAndLast([10])).toEqual([10, undefined]);
 	});
 });
 
-describe.skip('makeUnique', () => {
-	it.skip('Should return an array with unique items', () => {
+describe('makeUnique', () => {
+	it('Should return an array with unique items', () => {
 		const items = [0, 0, 0, 1, 2, 3, 'First', 'First', 1];
 		expect(array.makeUnique(items)).toEqual([0, 1, 2, 3, 'First']);
 	});
