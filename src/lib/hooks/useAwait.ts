9b3beee2452e2a06627da4a50f2cb362fc29011a
OGShawnLee <changoloco.evg@gmail.com>
2023-04-27 20:33:01 +0000

    fix(hooks/ts): ensure return type is a promise

--- src/lib/hooks/useAwait.ts
+++ src/lib/hooks/useAwait.ts
@@ -1,6 +1,8 @@
 import type { Result } from '$lib/types';
 
-export default async function useAwait<T, E = unknown>(fn: () => Promise<T>): Result<T, E> {
+export default async function useAwait<T, E = unknown>(
+	fn: () => Promise<T>
+): Promise<Result<T, E>> {
 	try {
 		return { failed: false, data: await fn() };
 	} catch (error) {
